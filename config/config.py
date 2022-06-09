c455a88158562a3e4bb9839049ba5d0c0edaab5f
Virtualunionsex <97935254+Virtualunionsex@users.noreply.github.com>
2022-06-10 02:37:47 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -69,10 +69,10 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/AyiinSupport"
+    "SUPPORT_CHANNEL", "https://t.me/storychopper"
 )  # Example:- https://t.me/AyiinSupport
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/AyiinXdSupport"
+    "SUPPORT_GROUP", "https://t.me/blackgallery_id"
 )  # Example:- https://t.me/AyiinXdSupport
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
