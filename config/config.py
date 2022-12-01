92ccf4e7ec845a0ed1f474e8130df6656cb56b51
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2022-12-01 22:00:25 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -69,11 +69,11 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/simpulyangrapuh"
-)  # Example:- https://t.me/simpulyangrapuh 
+    "SUPPORT_CHANNEL", "https://t.me/Nakama_asl"
+)  # Example:- https://t.me/nakama_asl
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/radiosimpulyangrapuh"
-)  # Example:- https://t.me/radiosimpulyangrapu
+    "SUPPORT_GROUP", "https://t.me/log_Azure"
+)  # Example:- https://t.me/Log_azurr
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
@@ -101,7 +101,7 @@ YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "3"))
 TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/virtualunionsex/KingMalik_MusicBot")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/virtualunionsex/onepiece")
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
